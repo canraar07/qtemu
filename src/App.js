031e8ac6d5ded15a193a84cab0c9f5f24dc85de4
canra <canraaripin7@gmail.com>
2019-08-24 08:43:20 +0700

    comit qtemu
    comit pertama

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import MaskedInput from 'react-text-mask';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      number: 0,
+      numberMultiply : 0
+    }
+  }
+
+  handlerDecrement = () =>{
+    this.setState({
+      number: this.state.number - 1
+    })
+  }
+
+  handlerInrement = () => {
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
+
+  haldleChange = (e) => {
+    this.setState({
+      numberMultiply : e.target.value
+    })
+  }
+
+  hendlerKali = () => {
+    this.setState({
+      number: this.state.number * this.state.numberMultiply
+    })
+  }
+
+  render(){
+  const { number } = this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Button style = {{marginRight : 10}}onClick={this.handlerInrement} className="coba" type="primary">
+         Tambah
+       </Button>
+       <Button onClick={this.handlerDecrement} className="coba" type="primary">
+         Kurang
+       </Button>
+       <br/>
+      <MaskedInput
+        mask={[ /[1-9]/, /\d/, /\d/,/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/]}
+        guide={false}
+        style = {{width : 100, marginRight : 5}}
+        onChange={this.haldleChange}/>
+       <Button style = {{marginTop : 5}}onClick={this.hendlerKali} className="coba" type="primary">
+         Kali
+       </Button>
+       <br/>
+       {number}
     </div>
   );
+}
 }
 
 export default App;
